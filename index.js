cdb1ed39665e7fd0124e8790d4a5f41fae8184d0
Aleksey Koldashov <123095853+AleksKoldashov@users.noreply.github.com>
2024-09-04 15:38:36 +0400

    исправлен

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const display = el("#display")
 const btnNum = el(".btn-num")
 const btnOper = el(".oper")
 
-let oneNum = ''
-let twoNum = ''
-let oper = ''
+var oneNum = ''
+var twoNum = ''
+var oper = ''
 
 function ArrowoneNum () {
     if(oper){
@@ -34,14 +34,26 @@ const setNum=function(){
         if(this.getAttribute("data-btn")=="." & twoNum ==''){
             twoNum += 0+this.getAttribute("data-btn");
             display.innerHTML = twoNum
+        }else if(this.getAttribute("data-btn")=="."){
+            let a = twoNum.indexOf('.')
+            if(a==-1){
+            twoNum += this.getAttribute("data-btn");
+            display.innerHTML = twoNum
+            }
         }else{
             twoNum += this.getAttribute("data-btn");
             display.innerHTML = twoNum
         }
     }else{
-        if(this.getAttribute("data-btn")=="."& oneNum ==''){
+        if(this.getAttribute("data-btn")=="." & oneNum ==''){
             oneNum += 0+this.getAttribute("data-btn");
             display.innerHTML = oneNum
+        }else if(this.getAttribute("data-btn")=="."){
+            let a = oneNum.indexOf('.')
+            if(a==-1){
+                oneNum += this.getAttribute("data-btn");
+                display.innerHTML =  oneNum
+            } 
         }else{
             oneNum += this.getAttribute("data-btn");
             display.innerHTML =  oneNum
@@ -53,53 +65,81 @@ const setOper=function(){
 oper = this.getAttribute("data-oper")
 }
 
-const result=()=>{
-    let res = ''
-    switch (oper) {
-        case "%":
-            if(twoNum=='0'){
-                res='странный выбор'
-                Clear()
-            }else{
-                res = parseFloat(oneNum) * (parseFloat(twoNum)/100)
-                Clear()
-            }
-            break;
-        case "+":
-            res = parseFloat(oneNum) + parseFloat(twoNum) 
-            Clear()
-            break;
-        case "-":
-            res = parseFloat(oneNum) - parseFloat(twoNum)
-            Clear() 
-            break;
-        case '*':
-            res = parseFloat(oneNum) * parseFloat(twoNum) 
-            Clear()
-            break;
-        case '/':
-            if(twoNum=='0'){
-                res = 'На ноль делить нельзя'
-                Clear()
-            }else{
-                res = parseFloat(oneNum) / parseFloat(twoNum) 
-                Clear()
-            }
-            break;
-        default:
-            break;
+class Result{
+    constructor(oneNum, twoNum){
+        this.oneNum = oneNum
+        this.twoNum = twoNum
     }
-    display.innerHTML = res
-    oneNum = ''
+getSum(){ 
+    let res = parseFloat(oneNum) + parseFloat(twoNum);
+    display.innerHTML = res;
     twoNum = ''
-    oper = ''
+    oneNum = res
 }
+getDif(){
+    let res = parseFloat(oneNum) - parseFloat(twoNum);
+    display.innerHTML = res
+    twoNum = ''
+    oneNum = res
+}
+getMul(){
+    let res = parseFloat(oneNum) * parseFloat(twoNum);
+    display.innerHTML = res;
+    twoNum = ''
+    oneNum = res
+}
+getDiv(){
+    let res = parseFloat(oneNum) / parseFloat(twoNum);
+    display.innerHTML = res;
+    twoNum = ''
+    oneNum = res
+}
+getPer(){
+    let res = parseFloat(oneNum) * (parseFloat(twoNum)/100);
+    display.innerHTML = res;
+    twoNum = ''
+    oneNum = res
+}
+}
+
+const calcul = new Result(oneNum, twoNum)
 
 const Clear=()=>{
     oneNum = ''
     twoNum = ''
     oper = ''
     display.innerHTML = '0'
+}
+
+const result=()=>{
+    switch (oper) {
+        case "%":
+            if(twoNum=='0'){
+                res='странный выбор'
+            }else{
+                calcul.getPer()
+            }
+            break;
+        case "+":
+            calcul.getSum()
+            break;
+        case "-":
+            calcul.getDif()
+            break;
+        case '*':
+            calcul.getMul()
+            break;
+        case '/':
+            if(twoNum=='0'){
+                res = 'На ноль делить нельзя'
+                Clear()
+            }else{
+            calcul.getDiv()
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 arrow.addEventListener("click", ArrowoneNum)
